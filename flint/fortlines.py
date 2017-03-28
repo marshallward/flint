36e6151634278ea37eb80962abb9b63bac53f3cd
Marshall Ward <marshall.ward@gmail.com>
2017-03-29 07:23:09 +1100

    Support line splitting by ;
    Basic support for split lines incorporated into the line iterator.

--- flint/fortlines.py
+++ flint/fortlines.py
@@ -2,8 +2,10 @@ class FortLines(object):
     """Fortran source line iterator"""
 
     def __init__(self, lines):
-        # TODO: Check if already an iterator?
         self.lines = iter(lines)
+
+        # Split lines
+        self.buffered_line = None
 
     def __iter__(self):
         return self
@@ -12,13 +14,24 @@ class FortLines(object):
         return self.__next__()
 
     def __next__(self):
-        line = next(self.lines)
+        if self.buffered_line:
+            line = self.buffered_line
+            self.buffered_line = None
+        else:
+            line = next(self.lines)
+
+        if ';' in line:
+            idx = line.index(';')
+            line, self.buffered_line = line[:idx], line[1 + idx:]
+
+            # How literally do we handle end-line semicolons?
+            # This tosses them aside
+            if self.buffered_line == []:
+                self.buffered_line = None
 
         # Join extended lines
+        # TODO: 255-limit line check
         while (line[-1] == '&'):
             line = line[:-1] + next(self.lines)
-
-        # TODO: Combine tokens incorrectly split by shlex
-        # (Or, you know, use a better tokenizer)
 
         return line
