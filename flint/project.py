f1d5d6208f811666fdf476ed671893e506ee6d6d
Marshall Ward <marshall.ward@gmail.com>
2020-05-21 12:18:33 -0400

    Temporarily support a variable list of paths
    I need a bit more control over my path selection for some of my tests,
    so I am currently allowing a variable argument list for each individual
    path.
    
    Generally I don't like these, especially for public APIs, so I don't
    expect this to persist for too long.

--- flint/project.py
+++ flint/project.py
@@ -13,21 +13,24 @@ class Project(object):
 
         self.directories = []
 
-    def parse(self, path):
-        assert os.path.isdir(path)
-        self.path = path
-
+    # TODO: *paths is generally a bad idea for a public API.  I am only using
+    #   it here to get sensible output in my MOM6 tests.
+    # TODO: `exclude` may prove more useful here.
+    def parse(self, *paths):
         filepaths = []
+        for path in paths:
+            assert os.path.isdir(path)
+            self.path = path
 
-        for root, dirs, files in os.walk(self.path):
-            self.directories.append(root)
+            for root, dirs, files in os.walk(self.path):
+                self.directories.append(root)
 
-            for fname in files:
-                fpath = os.path.join(root, fname)
-                if os.path.splitext(fname)[1] in ('.f90', '.F90'):
-                    filepaths.append(fpath)
-                else:
-                    print('SKIP file: {}'.format(fpath))
+                for fname in files:
+                    fpath = os.path.join(root, fname)
+                    if os.path.splitext(fname)[1] in ('.f90', '.F90'):
+                        filepaths.append(fpath)
+                    else:
+                        print('SKIP file: {}'.format(fpath))
 
         for fpath in filepaths:
             f90file = Source(project=self, verbose=self.verbose)
