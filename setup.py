f91561efa6e84ed409e86eb7f70516ec71fcfeb2
Marshall Ward <marshall.ward@gmail.com>
2021-02-15 14:41:25 -0500

    Setuptools (setup.py) install
    This patch adds a basic setuptools script for basic installation.
    
    It includes generation of the flint commandline tool, which currently
    only runs gendoc.

--- setup.py
+++ setup.py
@@ -9,33 +9,12 @@ except ImportError:
     from distutils.core import setup
     from distutils.core import Command
 
-#import tests.test_flint
-
 # Project details
 project_name = 'flint'
 project_version = __import__(project_name).__version__
 project_readme_fname = 'README.rst'
 project_pkgs = [path for (path, dirs, files) in os.walk(project_name)
                 if '__init__.py' in files]
-#project_scripts = [os.path.join('bin', f) for f in os.listdir('bin')]
-
-
-# Test suite
-#class ProjectTest(Command):
-#    user_options = []
-#
-#    def initialize_options(self):
-#        pass
-#
-#    def finalize_options(self):
-#        pass
-#
-#    def run(self):
-#        unittest = tests.test_flint.unittest
-#        testcase = tests.test_flint.Test
-#        suite = unittest.TestLoader().loadTestsFromTestCase(testcase)
-#        result = unittest.TextTestRunner(verbosity=2).run(suite)
-#        sys.exit(not result.wasSuccessful())
 
 
 # README
@@ -55,14 +34,11 @@ setup(
 
     packages = project_pkgs,
 
-    #scripts=project_scripts,
     entry_points={
         'console_scripts': [
             'flint = flint.cli:parse',
         ]
     },
-
-    #cmdclass = {'test': ProjectTest},
 
     classifiers = [
         'Development Status :: 1 - Planning',
